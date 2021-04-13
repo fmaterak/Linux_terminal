d45fc8144c5c625b7779554f7c52d32157a7cbad
fmaterak <62309735+fmaterak@users.noreply.github.com>
2021-04-13 02:55:17 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,97 +1,63 @@
-// g++ -Wall -Wextra main.cpp -lSDL2
 #include <cstdio>
 #include <errno.h>
+#include <cstdlib>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#include "term.h"
 
-int input_function (const char *text, void *data, char **output);
 
-int quit();
-
-int main (int argc, char **argv)
-{
-    SDL_Window   *window = NULL;
-    SDL_GLContext glContext = NULL;
-    Console_tty  *tty = NULL;
-    SDL_Event     event;
-
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <monospaced font path> <font size>\n", 
-                argv[0]);
-        exit(1);
+int main(){
+    int master= getpt();
+    if(master == -1){
+        perror("getpt");
+        return 1;
     }
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        fprintf(stderr, "SDL Failed to init: %s\n", SDL_GetError());
-        exit(1);
+    char slave_path[256]{};
+    if (ptsname_r(master, slave_path, sizeof(slave_path)) != 0){
+        perror("ptsname_r");
+        close(master);
+        return 2;
     }
 
-    /* Create a window that is resizeable. SDL_Console can handle resizing. */
-    window = SDL_CreateWindow("Console", 
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-        640, 480, 
-        SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
-
-    if (!window) {
-        fprintf(stderr, "Window could not be created: %s\n", SDL_GetError());
-        SDL_Quit();
-        exit(1);
+    if(grantpt(master) != 0){
+        perror("grantpt");
+        close(master);
+        return 3;
+    }
+    if(unlockpt(master) != 0){
+        perror("unlockpt");
+        close(master);
+        return 4;
     }
 
-    glContext = SDL_GL_CreateContext(window);
-    if (!glContext) {
-        printf("%s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        exit(1);
+    int slave = open(slave_path, O_RDWR);
+    if(slave < 0){
+        perror("open");
+        close(master);
+        return 2;
     }
 
-    tty = Console_Create(
-            window,         /* SDL_Window */
-            argv[1],        /* font path */
-            atoi(argv[2]),  /* font size */
-            SDLK_ESCAPE,    /* key that toggles console on/off */
-            input_function, /* function that handles text input to console */
-            NULL);          /* userdata to the above function */
-
-    if (!tty) {
-        fprintf(stderr, "Console could not init: %s\n", Console_GetError());
-        SDL_GL_DeleteContext(glContext);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        exit(1);
+    pid_t pid = fork();
+    if (pid == -1){
+        perror("fork");
+        return 5;
     }
+    if(pid == 0){
+        close(0);
+        close(1);
+        close(2);
+        close(3);
 
-    while(1) { 
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-            case SDL_QUIT:
-                return 0;
-                break;
-            }
-        }
 
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        dup2(slave, 0);
+        dup2(slave, 1);
+        dup2(slave, 2);
 
-        if (Console_Draw(tty)) { /* handle drawing the console if toggled */
-            fprintf(stderr, "%s\n", Console_GetError());
-            /* handle fatal console error */
-            Console_Destroy(tty);
-	    SDL_GL_DeleteContext(glContext);
-	    SDL_DestroyWindow(window);
-	    SDL_Quit();
-	    return 0;
-        }
+        close(slave);
 
-        SDL_GL_SwapWindow(window);
+        return 6;
     }
-
+    close(slave);
 } //main()
-
